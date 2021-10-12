5deb0993c66f65eb9ad1b88125a6bec8a352fb91
Greg <gkowal@protonmail.com>
2021-10-12 13:20:03 +0200

    added python dotenv config

--- API/jira_pomodoro/jira_object_wrapper.py
+++ API/jira_pomodoro/jira_object_wrapper.py
@@ -1,3 +1,8 @@
 from atlassian import Jira
-
-j = Jira('<instanceurl, username, apikey>')
+from dotenv import load_dotenv, find_dotenv
+import os
+load_dotenv(find_dotenv())
+API_KEY = os.environ.get("API_KEY")
+API_URL = os.environ.get("API_URL")
+API_USER = os.environ.get("API_USER")
+j = Jira( url=API_URL, username=API_USER, password=API_KEY)
